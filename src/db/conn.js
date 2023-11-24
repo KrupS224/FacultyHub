d0267cda88d50f4b19823ad308f225dc79ba15fb
Krupesh <krups224@gmail.com>
2023-11-24 17:13:05 +0530

    adding remaining files

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,57 +1,14 @@
-const Admin = require("../models/admin");
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+const mogoose = require('mongoose');  // require mongoose
+require('dotenv').config(); // require dotenv
 
-const requireAuth = (req, res, next) => {
-    const token = req.cookies.accesstoken;
-    if (token) {
-        jwt.verify(token, process.env.SECRET_KEY, (err, decodedToken) => {
-            if (err) {
-                console.log(err.message);
-                res.redirect("/signin");
-            } else {
-                // console.log(decodedToken);
-                next();
-            }
-        });
-    }
-    else {
-        res.redirect("/signin");
-    }
-}
+// Connect to database
+mogoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true
+}).then(() => {
+    console.log("Connection successful");
+}).catch((err) => {
+    console.log(`No connection, error: ${err}`);
+});
 
-const checkUser = (req, res, next) => {
-    const token = req.cookies.accesstoken;
-    if (token) {
-        jwt.verify(token, process.env.SECRET_KEY, async (err, decodedToken) => {
-            if (err) {
-                console.log(err.message);
-                res.locals.user = null;
-                next();
-            } else {
-                // console.log(decodedToken);
-                let user = await Admin.findById(decodedToken._id);
-                res.locals.user = user;
-                next();
-            }
-        });
-    }
-    else {
-        res.locals.user = null;
-        next();
-    }
-}
-
-const userDelete = async (email) => {
-    const user = await Admin.findOne({ email: email });
-    if (user.verified == 0) {
-        const deleteData = await Admin.deleteOne({ email: email });
-        console.log("User deleted successfully");
-    }
-}
-
-const adminLockUpdate = async (email) => {
-    await Admin.updateOne({ email: email }, { lock: false });
-}
-
-module.exports = { requireAuth, checkUser, userDelete, adminLockUpdate };
