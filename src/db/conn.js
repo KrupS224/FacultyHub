c782fce9db4c3c02b8df04d6abb6212d92c7797d
Krupesh <krups224@gmail.com>
2023-11-24 21:40:12 +0530

    changes for deployment

--- src/db/conn.js
+++ src/db/conn.js
@@ -6,7 +6,7 @@ require('dotenv').config(); // require dotenv
 const port = process.env.PORT || 8000;
 
 // Connect to database
-mogoose.connect(process.env.DATABASE_URL || "mongodb://localhost:27017/FacultyHub", {
+mogoose.connect(process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/FacultyHub", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // useCreateIndex: true
